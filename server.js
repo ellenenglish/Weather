1b65ba029e639234c024c2de909ab961455c5b8b
ellenenglish <ellenenglish@gmail.com>
2023-11-02 14:20:26 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+/* Empty JS object to act as endpoint for all routes */
+let projectData = {};
+
+// Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+/* Initializing the main project folder */
+app.use(express.static('website'));
+
+const port = 8000;
+const hostName = 'localHost';
+
+// TODO-Spin up the server
+const server = app.listen(port, listening);
+function listening() {
+    console.log('server running');
+    console.log(`running on localhost: ${port}`);
+};
+
+// GET route
+app.get('/storeData', (request, response)=>{
+    console.log("get", projectData);
+    response.send(projectData);
+});
+
+
+// POST route that adds incoming data to projectData
+app.post('/clientData', (request, response)=>{
+    projectData = request.body;
+    console.log("Post", projectData);
+    response.send(projectData);
+  });
+
+
+
---
+++ website/app.js
@@ -0,0 +1,77 @@
+/* Global Variables */
+
+
+// Personal API KEY For OpenWeatherMap API
+//units=metric to get Temp in celisus
+let apiKey = '&appid=fbd37568d48d1d282e9c662b54944c2b&units=imperial';
+
+// The URL TO retrieve weather information from API (Country ID US)
+let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+
+
+
+// Create a new date instance dynamically with JS
+let date = new Date();
+let newDate = date.toDateString();
+
+
+
+document.getElementById('generate').addEventListener('click', getUserInfo);
+
+function getUserInfo(e){
+    const zip =  document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+    getWeather(baseURL, zip, apiKey).then(function(info) {
+        postData('http://localhost:8000/clientData', {date: newDate, temp:info.main.temp, content:feelings, high:info.main.max, low: info.main.low})
+    })
+};
+
+
+const getWeather = async (baseURL, zip, apiKey)=>{
+  try {
+    const response = await fetch(baseURL+zip+apiKey);
+    const data = await response.json();
+    console.log(data)
+    return data;
+  }  catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+}
+
+const postData = async ( url = '', data = {})=>{
+    console.log(data);
+      const response = await fetch(url, {
+      method: 'POST', 
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+     // Body data type must match "Content-Type" header        
+      body: JSON.stringify(data), 
+    });
+  
+      try {
+        updateInfo()
+      }catch(error) {
+      console.log("error", error);
+      };
+  };
+
+
+  const updateInfo = async() => {
+    const request = await fetch('http://localhost:8000/storeData');
+    try{
+      const newData = await request.json;
+        document.getElementById('date').innerHTML = "Date: " + newData.date;
+        document.getElementById('temp').innerHTML = "Current Temperature: " + Math.round(newData.temp) + "&degC";
+        document.getElementById('feelings').innerHTML = "I feel : " + newData.content;
+
+   }catch(error){
+    console.log("error:", error);
+    // appropriately an error
+   };
+};
+
+
+
